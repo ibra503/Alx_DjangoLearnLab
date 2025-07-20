6e3bb3d29b7a64fc06f6876f15bef2e09543af02
Ibra <aouzalibrahim80@gmail.com>
2025-07-20 21:07:35 +0100

    relationship_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -12,7 +12,8 @@ books_by_author = Book.objects.filter(author=author)
 print("Books by John Doe:", books_by_author)
 
 
-library = Library.objects.get(name="liberary_name")
+library_name = "City Lib"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 print("Books in Central Library:", books_in_library)
 

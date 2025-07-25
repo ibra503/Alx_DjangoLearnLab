428672db4244274f6bf7f407764c22d6e6a14b84
Ibra <aouzalibrahim80@gmail.com>
2025-07-25 19:46:45 +0100

    views.urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import LibraryDetailView
 
 urlpatterns = [
-    path('list_books/', views.list_books, name='list_books'),  # Function-based view
-    path('library_list/', views.LibraryListView.as_view(), name='library_list'),  # Class-based view
+    path('list_books/', views.list_books, name='list_books'),
+    path('library_list/', views.library_list, name='library_list'),
+
+    # Class-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Book, Library
+from django.views.generic import DetailView
+from .models import Library
 
-# Function-based view
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-# Class-based view
-class LibraryListView(ListView):
+class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_list.html'
-    context_object_name = 'libraries'
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.book_set.all()
+        return context

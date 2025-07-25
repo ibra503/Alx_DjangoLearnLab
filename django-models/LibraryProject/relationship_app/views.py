ca1b343cf7a58cab15c9fdd4d094d2bb09bdc736
Ibra <aouzalibrahim80@gmail.com>
2025-07-25 19:58:01 +0100

    v

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-om django.shortcuts import render
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library  # Make sure this line is included!
 

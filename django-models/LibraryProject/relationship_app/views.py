6c99c5a431b73825149cf4e3a0396e4819cddd47
Ibra <aouzalibrahim80@gmail.com>
2025-07-25 20:17:16 +0100

    v

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # Ensure this line is included!
+from django.contrib.auth.models import User
+from .models import Author, Book, Library  # Added Library here!
 
 # Function-based view to list all books
 def list_books(request):
     """
-    Function-based view that displays all books in the database.
-    Renders a list of book titles and their authors.
+    Function-based view that lists all books stored in the database.
+    This view should render a simple text list of book titles and their authors.
     """
-    books = Book.objects.all()  # Get all books from database
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to display library details
+# Class-based view for library details
 class LibraryDetailView(DetailView):
     """
-    Class-based view that displays details for a specific library.
-    Shows the library name and all books available in that library.
+    Class-based view that displays details for a specific library, listing all books available
+    in that library. This view should render a page that shows the library name and 
+    all books available in that library.
     """
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
+    
     def get_context_data(self, **kwargs):
-        """
-        Override the get_context_data method to add the list of books
-        available in the library to the context.
-        """
         context = super().get_context_data(**kwargs)
-        context['books'] = self.object.book_set.all()  # Assuming a reverse relationship from Library to Book
+        # Additional context can be added here if needed
         return context

7e1e4c8b62f29abbcc53f38ef9f1f51c21636dab
Ibra <aouzalibrahim80@gmail.com>
2025-07-26 21:32:56 +0100

    .

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -13,4 +13,15 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-    
+    from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+    (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+(None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+admin.site.register(CustomUser, CustomUserAdmin)

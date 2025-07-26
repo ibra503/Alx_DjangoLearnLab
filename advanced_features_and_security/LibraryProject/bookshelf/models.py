1070018d0431d358994d16e197f161048367f48f
Ibra <aouzalibrahim80@gmail.com>
2025-07-26 21:28:48 +0100

    .

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,3 +7,10 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    

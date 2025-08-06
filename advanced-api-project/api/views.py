28b072924850563d5de0bbfac7d8b63626b74188
Ibra <aouzalibrahim80@gmail.com>
2025-08-06 20:56:15 +0100

    .

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 class BookListView(generics.ListCreateAPIView):
     """
     View for listing all books and creating new books

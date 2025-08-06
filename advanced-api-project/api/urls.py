88b7d79088a151f9fac55884cc66490a1a2ff3a7
Ibra <aouzalibrahim80@gmail.com>
2025-08-06 20:53:53 +0100

    .

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,5 +3,6 @@ from .views import BookListView, BookDetailView
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
+    "books/create", "books/update", "books/delete"
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]

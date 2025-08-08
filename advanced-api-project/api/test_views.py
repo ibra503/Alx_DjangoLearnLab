e163d7f8819fc6bc2f1f854670ad4e1eb0e4a8cc
Ibra <aouzalibrahim80@gmail.com>
2025-08-08 19:46:31 +0100

    adding missed moduls

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,6 +1,15 @@
-from dajango.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from django.urls import reverse
+from .models import Book  # Optional, if you use it
 
-    class APITestCase(TestCase):
-        def test_dummy(self):
-            selfasserEqual(1, 1)
-            
+class BookAPITests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('book-list')  # replace with your actual URL name
+
+    def test_book_list_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # This line checks for 'response.data'
+        self.assertIsNotNone(response.data)

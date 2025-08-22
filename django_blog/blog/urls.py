5266ab5194603911f46cc20d66d100cae819b8c2
Ibra <aouzalibrahim80@gmail.com>
2025-08-22 22:27:49 +0100

    .

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,3 +1,5 @@
 from django.urls import path
 [ "login/", "register/", "profile/"]
 post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/
+
+["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]

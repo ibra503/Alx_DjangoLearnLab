b24c23513eea0a41879ace8e28a98757b89f3655
Ibra <aouzalibrahim80@gmail.com>
2025-08-22 22:30:30 +0100

    .

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,3 +3,4 @@ from django.urls import path
 post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/
 
 ["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]
+tags/<slug:tag_slug>/", "PostByTagListView.as_view()"]

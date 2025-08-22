c22725fedb799a5d4d20d9d98523a4871b8188d1
Ibra <aouzalibrahim80@gmail.com>
2025-08-22 22:26:52 +0100

    .

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,3 +8,4 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+CommentForm(forms.ModelForm)", "model = Comment", "content"]

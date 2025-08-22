28300713decaa99f5d486f94a842f979df6efea2
Ibra <aouzalibrahim80@gmail.com>
2025-08-22 22:29:16 +0100

    .

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -9,3 +9,4 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 CommentForm(forms.ModelForm)", "model = Comment", "content"]
+TagWidget()", "tags", "widgets"]
